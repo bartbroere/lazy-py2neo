8761b7d2d9830644dd9b3169d3fd8bbef300bf93
Bart Broere <mail@bartbroere.eu>
2019-02-05 16:06:39 +0100

    Add derpy code

--- lazy_py2neo.py
+++ lazy_py2neo.py
@@ -1,8 +1,10 @@
-import py2neo
 from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
+
+import py2neo
 
 
-class Graph(py2neo.Graph):
+class Graph:
     """
     Neo4j queries often only use 1 CPU core.
     This simple wrapper can make it handle a
@@ -11,8 +13,30 @@ class Graph(py2neo.Graph):
     """
 
     def __init__(self, *a, concurrent_queries=1, **k):
-        self.graphs = ThreadPoolExecutor(max_workers=concurrent_queries)
-        super().__init__(*a, **k)
+        self.executor = ThreadPoolExecutor(max_workers=concurrent_queries)
+        self.graph = py2neo.Graph(*a, **k)
+        self.graphs = Queue()
+        for _ in range(concurrent_queries):
+            self.graphs.put(py2neo.Graph(*a, **k))
+        self.__dict__.update(
+            {parent_attribute: self.graph.__getattribute__(parent_attribute)
+             for parent_attribute in dir(self.graph)
+             if not parent_attribute.startswith('_')
+             and not parent_attribute in ['data', 'run']}
+        )
+
+    def data(self, *a, **k):
+        """
+        Does the same things as py2neo.Graph.run, only returns Futures
+        instead of results. Call .result() in the calling code to get
+        the actual output.
+        """
+        def graph_data(*a, **k):
+            graph = self.graphs.get(block=True)
+            data = graph.data(*a, **k)
+            self.graphs.put(graph)
+            return data
+        return self.executor.submit(graph_data, *a, **k)
 
     def run(self, *a, **k):
         """
@@ -20,4 +44,9 @@ class Graph(py2neo.Graph):
         instead of results. Call .result() in the calling code to get
         the actual output.
         """
-        return self.graphs.submit(super().run, *a, **k)
+        def graph_run(*a, **k):
+            graph = self.graphs.get(block=True)
+            data = graph.run(*a, **k)
+            self.graphs.put(graph)
+            return data
+        return self.executor.submit(graph_run, *a, **k)
