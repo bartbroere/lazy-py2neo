fee46d78891e47a69beff10fd7c355c9af6ccbc4
Bart Broere <mail@bartbroere.eu>
2019-02-08 14:16:32 +0100

    Fix documentation and ``not in`` condition

--- lazy_py2neo.py
+++ lazy_py2neo.py
@@ -6,21 +6,13 @@ import py2neo
 
 class Graph:
     """
-    Neo4j queries often only use one CPU core on the database server.
-    This simple wrapper can make it handle a defined maximum of concurrent
-    queries. It's not very nice, but it could speed things up.
+    Neo4j queries often only use 1 CPU core.
+    This simple wrapper can make it handle a
+    defined maximum of concurrent queries.
+    It's not very nice, but it could speed things up.
     """
 
     def __init__(self, *a, concurrent_queries=1, **k):
-        """
-        Get database connectors for the amount defined in concurrent_queries.
-        Do a poor man's subclass.
-
-        :param a: positional arguments for py2neo.Graph()
-        :param concurrent_queries: number of concurrent queries and database
-                                   connections
-        :param k: keyword arguments for py2neo.Graph()
-        """
         self.executor = ThreadPoolExecutor(max_workers=concurrent_queries)
         self.graph = py2neo.Graph(*a, **k)
         self.graphs = Queue()
@@ -30,12 +22,12 @@ class Graph:
             {parent_attribute: self.graph.__getattribute__(parent_attribute)
              for parent_attribute in dir(self.graph)
              if not parent_attribute.startswith('_')
-             and not parent_attribute in ['data', 'run']}
+             and parent_attribute not in ['data', 'run']}
         )
 
     def data(self, *a, **k):
         """
-        Does the same things as py2neo.Graph.data, only returns Futures
+        Does the same things as py2neo.Graph.run, only returns Futures
         instead of results. Call .result() in the calling code to get
         the actual output.
         """
