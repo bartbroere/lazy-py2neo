e3fc5fbd4c2328e4b8d82769d7eafdd3af1a4b84
Bart Broere <mail@bartbroere.eu>
2019-02-12 15:50:50 +0100

    By default, mimic py2neo.Graph

--- lazy_py2neo.py
+++ lazy_py2neo.py
@@ -19,34 +19,59 @@ class Graph:
         for _ in range(concurrent_queries):
             self.graphs.put(py2neo.Graph(*a, **k))
         self.__dict__.update(
-            {parent_attribute: self.graph.__getattribute__(parent_attribute)
+            {parent_attribute: getattr(self.graph, parent_attribute)
              for parent_attribute in dir(self.graph)
              if not parent_attribute.startswith('_')
              and parent_attribute not in ['data', 'run']}
         )
 
-    def data(self, *a, **k):
+    def data(self, *a, as_future=False, **k):
         """
-        Does the same things as py2neo.Graph.run, only returns Futures
+        Does the same things as py2neo.Graph.data, but can return Futures
         instead of results. Call .result() in the calling code to get
         the actual output.
         """
+        if not as_future:
+            return self.graph.data(*a, **k)
+
         def graph_data(*a, **k):
             graph = self.graphs.get(block=True)
             data = graph.data(*a, **k)
             self.graphs.put(graph)
             return data
+
         return self.executor.submit(graph_data, *a, **k)
 
-    def run(self, *a, **k):
+    def run(self, *a, as_future=False, **k):
         """
-        Does the same things as py2neo.Graph.run, only returns Futures
+        Does the same things as py2neo.Graph.run, but can return Futures
         instead of results. Call .result() in the calling code to get
         the actual output.
         """
+        if not as_future:
+            return self.graph.run(*a, **k)
+
         def graph_run(*a, **k):
             graph = self.graphs.get(block=True)
             data = graph.run(*a, **k)
             self.graphs.put(graph)
             return data
+
         return self.executor.submit(graph_run, *a, **k)
+
+    def evaluate(self, *a, as_future=False, **k):
+        """
+        Does the same things as py2neo.Graph.evaluate, but can return Futures
+        instead of results. Call .result() in the calling code to get
+        the actual output.
+        """
+        if not as_future:
+            return self.graph.evaluate(*a, **k)
+
+        def graph_evaluate(*a, **k):
+            graph = self.graphs.get(block=True)
+            data = graph.evaluate(*a, **k)
+            self.graphs.put(graph)
+            return data
+        
+        return self.executor.submit(graph_evaluate, *a, **k)
